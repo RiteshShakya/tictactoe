824e3757c36f7c86157e3e206f28b11d239d1491
RiteshShakya <shakyarites07@gmail.com>
2021-09-18 21:57:55 +0530

    styles
    sass added and styled the app a bit

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,8 @@
 import React from 'react'
 
 const Square = ({value}) => {
-    // console.log(props);
 
-    return  <button type="button"> {value} </button>
+    return  <button type="button" className="square"> {value} </button>
     
 };
 
